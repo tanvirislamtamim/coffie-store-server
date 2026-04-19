7c45e5636839e3ee97fa8c2f7e65a21fd8d263ea
Your Name <you@example.com>
2026-04-19 06:22:03 +0600

    user updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 console.log(process.env.DB_USER)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yizffch.mongodb.net/?appName=Cluster0`;
-// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.yizffch.mongodb.net/?appName=Cluster0";
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,6 +31,7 @@ async function run() {
   try {
 
     const coffeeCollection = client.db('coffeeDB').collection('coffees')
+    const usersCollection = client.db('coffeeDB').collection('users')
 //  get kortee
     app.get('/coffees', async(req,res)=>{
       const result = await coffeeCollection.find().toArray();
@@ -69,6 +70,39 @@ async function run() {
       const result = await coffeeCollection.deleteOne(query);
       res.send(result);
     })
+
+    // User related apis
+    app.get('/users', async(req,res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+
+    app.post('/users', async(req,res)=>{
+      const users = req.body;
+      const result = usersCollection.insertOne(users);
+      res.send(result);
+    })
+
+    app.patch('/users',async(req,res)=>{
+      const {email,lastSignInTime } =req.body;
+      const filter = {email: email }
+      const upDatedDocs = {
+        $set: {
+          lastSignInTime: lastSignInTime
+        }
+      }
+      const result = await usersCollection.updateOne(filter, upDatedDocs)
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
 
     // Connect the client to the server	(optional starting in v4.7)
